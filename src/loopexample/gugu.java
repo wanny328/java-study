7ab5e1109fc3977d6e8db3cceb4b9a841f4479ab
wanny328 <joseph01756@gmail.com>
2022-06-11 15:04:56 +0900

    짝수단 출력 프로그램

--- src/loopexample/gugu.java
+++ src/loopexample/gugu.java
@@ -1,6 +1,6 @@
 package loopexample;
 
-public class gugu {
+public class Gugu {
 	public static void main(String[] args) {
 		System.out.println("짝수단 출력 프로그래밍");
 		for(int i = 2; i < 10; i++) {
--- src/loopexample/gugu2.java
+++ src/loopexample/gugu2.java
@@ -1,13 +1,14 @@
 package loopexample;
 
-public class gugu2 {
+public class Gugu2 {
 	public static void main(String[] args) {
 		System.out.println("짝수단 출력 프로그래밍");
 		for(int i = 2; i < 10; i++) {
+			if(i % 2 == 1) {
+				continue;
+			}
 			for(int j = 1; j < 10; j++) {
-				if(j <= i) {
 				System.out.println(i + " x " + j + " = " + (i * j));
-				}
 			}
 			System.out.println();
 		}
