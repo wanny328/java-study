accf531f747995849fd5f22cd09a9b1d8729c1b7
wanny328 <joseph01756@gmail.com>
2022-06-11 14:46:01 +0900

    upload easy gugu program

---
+++ src/loopexample/gugu.java
@@ -0,0 +1,16 @@
+package loopexample;
+
+public class gugu {
+	public static void main(String[] args) {
+		System.out.println("짝수단 출력 프로그래밍");
+		for(int i = 2; i < 10; i++) {
+			if(i % 2 == 1) {
+				continue;
+			}
+			for(int j = 1; j < 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+			System.out.println();
+		}
+	}
+}
